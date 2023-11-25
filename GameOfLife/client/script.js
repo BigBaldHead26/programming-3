8a94dad200ae8e8f158320f168442834f9317128
Hov_909 <hovhannes.h24@tumo.org>
2023-11-25 11:20:33 +0400

    statistics, changed backends

--- GameOfLife/client/script.js
+++ GameOfLife/client/script.js
@@ -1,5 +1,6 @@
 var socket = io();
 let side = 25;
+let weather = "spring";
 
 
 function setup() {
@@ -10,31 +11,102 @@ function setup() {
 function paint(matrix) {
         for (var y = 0; y < matrix.length; y++) {
                 for (var x = 0; x < matrix[y].length; x++) {
-                    var toBot = side - side * 0.3
+                    var toBot = side - side * 0.3;
                     textSize(toBot);
                     if (matrix[y][x] == 1) {
-                        fill("green");
-                        rect(x * side, y * side, side, side);
-                        text('☘️', x * side, y * side + toBot);
+                        if(weather == "spring"){
+                            fill("green");
+                            rect(x * side, y * side, side, side);
+                            text('☘️', x * side, y * side + toBot);
+                        }else if(weather == "summer"){
+                            fill("yellow");
+                            rect(x * side, y * side, side, side);
+                            text('🌼', x * side, y * side + toBot);
+                        }else if(weather == "winter"){
+                            fill("#00BDFF");
+                            rect(x * side, y * side, side, side);
+                            text('❄️', x * side, y * side + toBot);
+                        }else if(weather == "autumn"){
+                            fill("#FFBF00");
+                            rect(x * side, y * side, side, side);
+                            text('🍁', x * side, y * side + toBot);
+                        }
+              
                     }
                     else if (matrix[y][x] == 2) {
-                        fill("yellow");
-                        rect(x * side, y * side, side, side);
-                        text('🍂', x * side, y * side + toBot);
+                        if(weather == "spring"){
+                            fill("yellow");
+                            rect(x * side, y * side, side, side);
+                            text('🍂', x * side, y * side + toBot);
+                        }else if(weather == "summer"){
+                            fill("BlanchedAlmond");
+                            rect(x * side, y * side, side, side);
+                            text('🥀', x * side, y * side + toBot);
+                        }else if(weather == "winter"){
+                            fill("RoyalBlue");
+                            rect(x * side, y * side, side, side);
+                            text('🌾', x * side, y * side + toBot);
+                        }else if(weather == "autumn"){
+                            fill("Brown");
+                            rect(x * side, y * side, side, side);
+                            text('🕸', x * side, y * side + toBot);
+                        }
                     }
                     else if (matrix[y][x] == 3) {
-                        fill("red");
-                        rect(x * side, y * side, side, side);
-                        text('💥', x * side, y * side + toBot);
+                        if(weather == "spring"){
+                            fill("red");
+                            rect(x * side, y * side, side, side);
+                            text('💥', x * side, y * side + toBot);
+                        }else if(weather == "summer"){
+                            fill("darkred");
+                            rect(x * side, y * side, side, side);
+                            text('🔥', x * side, y * side + toBot);
+                        }else if(weather == "winter"){
+                            fill("Crimson");
+                            rect(x * side, y * side, side, side);
+                            text('☄️', x * side, y * side + toBot);
+                        }else if(weather == "autumn"){
+                            fill("DarkKhaki");
+                            rect(x * side, y * side, side, side);
+                            text('⚡️', x * side, y * side + toBot);
+                        }
                     }else if(matrix[y][x] == 4){
-                        fill("purple");
-                        rect(x * side, y * side, side, side);
-                        text('🍇', x * side, y * side + toBot);
+                        if(weather == "spring"){
+                            fill("purple");
+                            rect(x * side, y * side, side, side);
+                            text('🍇', x * side, y * side + toBot);
+                        }else if(weather == "summer"){
+                            fill("DarkMagenta");
+                            rect(x * side, y * side, side, side);
+                            text('🌺', x * side, y * side + toBot);
+                        }else if(weather == "winter"){
+                            fill("DarkSlateBlue");
+                            rect(x * side, y * side, side, side);
+                            text('☂️', x * side, y * side + toBot);
+                        }else if(weather == "autumn"){
+                            fill("DeepPink");
+                            rect(x * side, y * side, side, side);
+                            text('👾', x * side, y * side + toBot);
+                        }
                     }
                     else if(matrix[y][x] == 5){
-                        fill("aqua");
-                        rect(x * side, y * side, side, side);
-                        text('🌪', x * side, y * side + toBot);
+                        if(weather == "spring"){
+                            fill("aqua");
+                            rect(x * side, y * side, side, side);
+                            text('🌪', x * side, y * side + toBot);
+                        }else if(weather == "summer"){
+                            fill("LightBlue");
+                            rect(x * side, y * side, side, side);
+                            text('☁️', x * side, y * side + toBot);
+                        }else if(weather == "winter"){
+                            fill("LightCyan");
+                            rect(x * side, y * side, side, side);
+                            text('💨', x * side, y * side + toBot);
+                        }else if(weather == "autumn"){
+                            fill("LightSlateGrey");
+                            rect(x * side, y * side, side, side);
+                            text('🌧', x * side, y * side + toBot);
+                        }
                     }
                     
                     else {
@@ -52,3 +124,30 @@ setInterval(
         socket.on('send matrix', paint)
         },1000
     )
+
+const summer = document.querySelector('#summerid');
+const winter = document.querySelector('#winterid');
+const spring = document.querySelector('#springid');
+const autumn = document.querySelector('#autumnid');
+
+
+summer.addEventListener('click', () => {
+    weather = "summer";
+    socket.emit('Summer')
+});
+
+winter.addEventListener('click', () => {
+    weather = "winter";
+    socket.emit('Winter')
+});
+
+spring.addEventListener('click', () => {
+    weather = "spring";
+    socket.emit('Spring')
+});
+
+autumn.addEventListener('click', () => {
+    weather = "autumn";
+    socket.emit('Autumn')
+});
+
